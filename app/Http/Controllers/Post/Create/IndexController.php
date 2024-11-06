eca39b92d1d7fabc6d64f9169e3994d8ddad5d15
TakayukiHirano117 <ikusa.hirano@icloud.com>
2024-11-06 22:41:13 +0900

    Created some Controllers and Views and Installed Breeze.

---
+++ app/Http/Controllers/Post/Create/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Post\Create;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return view('posts.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Post;
+use App\Http\Controllers\TopController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', TopController::class)->name('top');
+    Route::get('/posts', Post\IndexController::class)->name('posts.index');
+    Route::get('/posts/{id}', Post\ShowController::class)->name('posts.show');
+    Route::get('/posts/{id}/edit', Post\Edit\IndexController::class)->name('posts.edit');
+    Route::get('/posts/create', Post\Create\IndexController::class)->name('posts.create');
+    Route::post('/posts', Post\Create\StoreController::class)->name('posts.store');
+    Route::put('/posts/{id}', Post\Edit\UpdateController::class)->name('posts.update');
+    Route::delete('/posts/{id}', Post\Delete\DestroyController::class)->name('posts.destroy');
 });
+
+require __DIR__.'/auth.php';
